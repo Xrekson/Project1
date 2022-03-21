6555bb568bc43ea62e686b1c0316440316a0e0f0
Xrekson <76116355+Xrekson@users.noreply.github.com>
2022-03-21 22:49:01 +0530

    files uploaded
    1st change

---
+++ prime.js
@@ -0,0 +1,7 @@
+function dis(){
+    document.getElementById("1").disabled = true;
+    document.getElementById("2").disabled = true;
+    document.getElementById("3").disabled = true;
+    document.getElementById("4").disabled = true;
+    $("div").css('background-color','green');
+}
